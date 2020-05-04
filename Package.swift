f23156e313c6085f4397f3941c6ea2c913eee756
RoRoGadget <44232777+RoRoGadget@users.noreply.github.com>
2020-05-04 11:51:09 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "postgres-kit",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_15),
+       .iOS(.v13)
     ],
     products: [
         .library(name: "PostgresKit", targets: ["PostgresKit"]),
